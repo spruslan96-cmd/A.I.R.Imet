c671a8d11d9e7db560d1d06dc4f7e1d479b8f997
agamairi <agam.airi@gmail.com>
2025-01-28 15:55:15 -0500

    adding model check page to the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:local_ai_chat/Bloc/Bloc/model_bloc.dart';
+import 'package:local_ai_chat/Bloc/Events/model_events.dart';
+import 'package:local_ai_chat/Screens/model_check_page.dart';
 
+void main() {
+  runApp(const ChatAIApp());
+}
+
+class ChatAIApp extends StatelessWidget {
+  const ChatAIApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat with the AI',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: BlocProvider(
+        create: (context) => ModelBloc()..add(CheckModelEvent()),
+        child: const ModelCheckPage(),
+      ),
+    );
+  }
+}
