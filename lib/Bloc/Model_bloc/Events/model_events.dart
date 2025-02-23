811f867959e5192a2297db19a54051b3c0929c94
agamairi <agam.airi@gmail.com>
2025-02-23 14:02:18 -0500

    removed bloc files

--- lib/Bloc/Model_bloc/Events/model_events.dart
+++
@@ -1,9 +0,0 @@
-// Bloc Events
-abstract class ModelEvent {}
-
-class CheckModelEvent extends ModelEvent {}
-
-class DownloadModelEvent extends ModelEvent {
-  final String modelSize;
-  DownloadModelEvent(this.modelSize);
-}
