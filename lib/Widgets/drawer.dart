cb401b05abb2ca1dd09638ecbbf3477309f5cd45
agamairi <agam.airi@gmail.com>
2025-02-23 14:04:48 -0500

    created a simple drawer

---
+++ lib/Widgets/drawer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:local_ai_chat/Screens/ai_chat_page.dart';
+import 'package:local_ai_chat/Screens/manage_models.dart';
+import 'package:local_ai_chat/Screens/model_check_page.dart';
+
+class Hamburger extends StatelessWidget {
+  const Hamburger({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            child: Container(
+              color: Colors.pink,
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                return const ChatPage();
+              }));
+            },
+            child: Text('chat Page'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                return const ModelCheckPage();
+              }));
+            },
+            child: Text('Model Page'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                return const ManageModelsPage();
+              }));
+            },
+            child: Text('Model Manage'),
+          ),
+        ],
+      ),
+    );
+  }
+}
