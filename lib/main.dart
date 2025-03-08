d3b61f4ca82ab0a2be31c0ed0acb71c67fdd28a4
agamairi <agam.airi@gmail.com>
2025-03-08 01:26:00 -0500

    added trial main.dart for provider

--- lib/main.dart
+++ lib/main.dart
@@ -19,3 +19,37 @@ class ChatAIApp extends StatelessWidget {
     );
   }
 }
+
+// // main.dart
+// import 'package:flutter/material.dart';
+// import 'package:local_ai_chat/Screens/chat_page.dart';
+// import 'package:provider/provider.dart';
+// import 'package:local_ai_chat/providers/chat_provider.dart';
+// import 'package:local_ai_chat/providers/talk_provider.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   runApp(
+//     MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(create: (context) => ChatProvider()),
+//         ChangeNotifierProvider(create: (context) => TalkProvider()),
+//       ],
+//       child: const ChatAIApp(),
+//     ),
+//   );
+// }
+
+// class ChatAIApp extends StatelessWidget {
+//   const ChatAIApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Chat with the AI',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: const ChatPage(),
+//     );
+//   }
+// }
