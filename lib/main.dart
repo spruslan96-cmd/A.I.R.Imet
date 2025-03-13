94b5aec8d7e73b45efc8961243ed08be71f9b4c8
agamairi <agam.airi@gmail.com>
2025-03-12 21:09:00 -0400

    updated to use theme provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,54 @@
+// import 'package:flutter/material.dart';
+// import 'package:local_ai_chat/Screens/chat_page.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   runApp(const ChatAIApp()); // Run the app
+// }
+
+// class ChatAIApp extends StatelessWidget {
+//   const ChatAIApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Chat with the AI',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: const ChatPage(), // Go to your ChatPage (UI)
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:local_ai_chat/Screens/chat_page.dart';
+import 'package:provider/provider.dart';
+import 'providers/theme_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(const ChatAIApp()); // Run the app
+  final themeProvider = ThemeProvider();
+  runApp(MyApp(themeProvider: themeProvider));
 }
 
-class ChatAIApp extends StatelessWidget {
-  const ChatAIApp({super.key});
+class MyApp extends StatelessWidget {
+  final ThemeProvider themeProvider;
+  const MyApp({super.key, required this.themeProvider});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chat with the AI',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const ChatPage(), // Go to your ChatPage (UI)
+    return ChangeNotifierProvider(
+      create: (_) => themeProvider,
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'AI Chat',
+            theme: themeProvider.themeData,
+            home: const ChatPage(),
+          );
+        },
+      ),
     );
   }
 }
