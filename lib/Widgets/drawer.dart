94fecc1e2e356ca57e2664de9be3dd554900c967
agamairi <agam.airi@gmail.com>
2025-06-24 18:46:57 -0400

    Fix: design changes

--- lib/Widgets/drawer.dart
+++ lib/Widgets/drawer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:local_ai_chat/Screens/chat_page.dart';
-import 'package:local_ai_chat/Screens/manage_models.dart';
+// import 'package:local_ai_chat/Screens/manage_models.dart';
 import 'package:local_ai_chat/Screens/model_download.dart';
 import 'package:local_ai_chat/Screens/settings_page.dart';
 import 'package:local_ai_chat/Screens/talk_page.dart';
@@ -12,60 +12,100 @@ class Hamburger extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: [
-          DrawerHeader(
-            child: Container(
-              color: Colors.pink,
+    final theme = Theme.of(context);
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: theme.colorScheme.primary,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'A.I.R.I',
+                      style: TextStyle(
+                        color: theme.colorScheme.onPrimary,
+                        fontSize: 20,
+                        // fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'AI, Real-time, In-App',
+                      style: TextStyle(
+                        color: theme.colorScheme.onPrimary,
+                        fontSize: 12,
+                        // fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (BuildContext context) {
-                return const ChatPage();
-              }));
-            },
-            child: Text('Chat Page'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (BuildContext context) {
-                return const TalkPage();
-              }));
-            },
-            child: Text('Talk Page'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (BuildContext context) {
-                return const ModelCheckPage();
-              }));
-            },
-            child: Text('Model Page'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (BuildContext context) {
-                return const ManageModelsPage();
-              }));
-            },
-            child: Text('Model Manage'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (BuildContext context) {
-                return const SettingsPage();
-              }));
-            },
-            child: Text('Settings'),
-          ),
-        ],
+            ListTile(
+              leading: Icon(
+                Icons.mark_chat_unread_outlined,
+                size: 32,
+                color: theme.colorScheme.primary,
+              ),
+              title: const Text('Chat with AI'),
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return const ChatPage();
+                }));
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.mic_none_outlined,
+                size: 32,
+                color: theme.colorScheme.primary,
+              ),
+              title: const Text('Talk to AI'),
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return const TalkPage();
+                }));
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.model_training,
+                size: 32,
+                color: theme.colorScheme.primary,
+              ),
+              title: const Text('Manage Models'),
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return const ModelCheckPage();
+                }));
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings_outlined,
+                size: 32,
+                color: theme.colorScheme.primary,
+              ),
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return const SettingsPage();
+                }));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
