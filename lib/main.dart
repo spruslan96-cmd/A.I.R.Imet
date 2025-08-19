3d30e74ee702028891c9ca6eb68cdec098486bca
agamairi <agam.airi@gmail.com>
2025-08-18 20:46:37 -0400

    removed comments, changed title to 'A.I.R.I'

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:local_ai_chat/Screens/chat_page.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   runApp(const ChatAIApp()); // Run the app
-// }
-
-// class ChatAIApp extends StatelessWidget {
-//   const ChatAIApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Chat with the AI',
-//       theme: ThemeData(primarySwatch: Colors.blue),
-//       home: const ChatPage(), // Go to your ChatPage (UI)
-//     );
-//   }
-// }
-
+// main.dart
 import 'package:flutter/material.dart';
 import 'package:local_ai_chat/Screens/chat_page.dart';
 import 'package:provider/provider.dart';
@@ -27,12 +6,14 @@ import 'providers/theme_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   final themeProvider = ThemeProvider();
   runApp(MyApp(themeProvider: themeProvider));
 }
 
 class MyApp extends StatelessWidget {
   final ThemeProvider themeProvider;
+
   const MyApp({super.key, required this.themeProvider});
 
   @override
@@ -43,7 +24,7 @@ class MyApp extends StatelessWidget {
         builder: (context, themeProvider, child) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            title: 'AI Chat',
+            title: 'A.I.R.I',
             theme: themeProvider.themeData,
             home: const ChatPage(),
           );
@@ -52,37 +33,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// // main.dart
-// import 'package:flutter/material.dart';
-// import 'package:local_ai_chat/Screens/chat_page.dart';
-// import 'package:provider/provider.dart';
-// import 'package:local_ai_chat/providers/chat_provider.dart';
-// import 'package:local_ai_chat/providers/talk_provider.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   runApp(
-//     MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider(create: (context) => ChatProvider()),
-//         ChangeNotifierProvider(create: (context) => TalkProvider()),
-//       ],
-//       child: const ChatAIApp(),
-//     ),
-//   );
-// }
-
-// class ChatAIApp extends StatelessWidget {
-//   const ChatAIApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Chat with the AI',
-//       theme: ThemeData(primarySwatch: Colors.blue),
-//       home: const ChatPage(),
-//     );
-//   }
-// }
